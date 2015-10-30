1bc7447d98db08d723a508f55b0c37ea279f2064
charroux <benoit.charroux@free.fr>
2015-10-30 11:13:43 +0100

    first commit

---
+++ src/tp1/Calc.java
@@ -0,0 +1,17 @@
+package tp1;
+
+import java.util.Scanner;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Entier 1 : ");
+		int i1 = scanner.nextInt();
+		System.out.println("Entier 2 : ");
+		int i2 = scanner.nextInt();
+		
+	}
+
+}
