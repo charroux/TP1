608ee714511072f8aca4c74b6f29cae55792046e
charroux <benoit.charroux@free.fr>
2015-10-30 11:30:58 +0100

    ajout calcul

--- src/tp1/Calc.java
+++ src/tp1/Calc.java
@@ -12,6 +12,22 @@ public class Calc {
 		System.out.println("Entier 2 : ");
 		int i2 = scanner.nextInt();
 		
+		int somme = i1 + i2;
+		
+		System.out.println("La somme est : " + somme);
+		
+		int q = i1 / i2;
+		
+		System.out.println("Quotien ? " + q);
+		
+		float quotien = (float)i1 / (float)i2;
+		
+		System.out.println("Le quotien est : " + quotien);
+		
+		int reste = i1 % i2;
+		
+		System.out.println("Le reste est : " + reste);		
+		
 	}
 
 }
